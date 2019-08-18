25e03149884ce1700570be00f777a2464d1d15bf
ShikharSundriyal <shikhar.sundriyal@gmail.com>
2019-08-18 21:11:18 +0530

    Initial Commit

--- src/main/scala/com/ss/SparkQuestion5.scala
+++ src/main/scala/com/ss/SparkQuestion5.scala
@@ -1,5 +1,7 @@
 package com.ss
-
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.row_number
+import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 //PROBLEM Statement : Find the lowest salary in each department
 object SparkQuestion5 {
@@ -10,13 +12,14 @@ object SparkQuestion5 {
     sparkSession.sparkContext.setLogLevel("error")
 
     import sparkSession.implicits._
-    import org.apache.spark.sql.expressions.Window
-    import org.apache.spark.sql.functions.row_number
-    import org.apache.spark.sql.functions._
-    val df=Seq((1,100),(1,200),(2,300),(2,50),(2,50)).toDF("id","amount")
-    val parti=Window.partitionBy($"amount")
-    df.withColumn("rank",dense_rank().over(Window.partitionBy($"id").orderBy($"amount".desc)))
+
+    val df=Seq((1,100),(1,200),(2,300),(2,50),(3,50)).toDF("id","amount")
+    //Dataframes way
+    val window=Window.partitionBy($"id").orderBy($"amount".asc,$"id".asc)
+    val df1=df.withColumn("row_number",row_number().over(window))
+    df1.select("id","amount").where("row_number=1").show()
+  //Sparl SQL WAY
     df.createOrReplaceTempView("test")
-    sparkSession.sql("select * from (select id ,amount , dense_rank() over(partition by id order by amount) as drank from test) where drank=1").show()
+    sparkSession.sql("select id, amount from (select id ,amount , row_number() over(partition by id order by amount asc, id asc) as rnumber from test) where rnumber=1").show()
   }
 }
