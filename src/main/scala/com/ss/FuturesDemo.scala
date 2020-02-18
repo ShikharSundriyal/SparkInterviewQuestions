f4c8816f39a9c955ef6053c7bf0011297e4374c9
ShikharSundriyal <shikhar.sundriyal@gmail.com>
2020-02-19 01:18:47 +0530

    Parallel run based on two path list

--- src/main/scala/com/ss/FuturesDemo.scala
+++ src/main/scala/com/ss/FuturesDemo.scala
@@ -7,24 +7,46 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 object FuturesDemo {
   def main(args: Array[String]): Unit = {
-    val path1 = "C:\\Users\\Shikhar\\Desktop\\input.txt,C:\\Users\\Shikhar\\Desktop\\input1.txt,C:\\Users\\Shikhar\\Desktop\\input2.txt"
-    //val path2 = "C:\\Users\\Shikhar\\Desktop\\input1.txt"
-    //val path3 = "C:\\Users\\Shikhar\\Desktop\\input2.txt"
-
-    val lis = path1.split(",")
-    lis.foreach(source => donutStock(source).onComplete {
-      case Success(stock) => println(s"Stock for vanilla donut = $stock")
-      case Failure(e) => println(s"Failed to find vanilla donut stock, exception = $e")
-    })
-    Thread.sleep(9000)
-    println("I am at the exit")
-
-    def donutStock(donut: String): Future[Unit] = Future {
+    def donutStock(donut: String): Future[Int] = Future {
       // assume some long running database operation
       for(line<-Source.fromFile(donut).getLines()){println(line)
-      Thread.sleep(300)}
+        Thread.sleep(200)}
+      1
 
     }
+    val path1 = "C:\\Users\\Shikhar\\Desktop\\input.txt,C:\\Users\\Shikhar\\Desktop\\input1.txt,C:\\Users\\Shikhar\\Desktop\\input2.txt,C:\\Users\\Shikhar\\Desktop\\input6.txt"
+    val path2 = "C:\\Users\\Shikhar\\Desktop\\input4.txt,C:\\Users\\Shikhar\\Desktop\\input5.txt"
+    //val path3 = "C:\\Users\\Shikhar\\Desktop\\input2.txt"
+    var first_list=0
+    val lis = path1.split(",")
+
+    val u=lis.foreach(source => donutStock(source).onComplete {
+      case Success(y) => {first_list+=y
+      println("succefully loaded " + source)}
+      case Failure(e) => println(s"Failed to load, exception = $e")
+    })
+
+
+    Thread.sleep(10000)//Time Taken to load path1 tables
+
+    val lis2 = path2.split(",")
+    var second_list=0
+    if(first_list==lis.length){
+      println("DATA LOADED FOR PATH1 , NOW BEGINNING PATH2 LOAD")
+      lis2.foreach(source => donutStock(source).onComplete {
+        case Success(x) => {second_list+=x
+        println("Data Loaded for"+source)}
+        case Failure(e) => println(s"Failed to load, exception = $e")
+      })
+    }
+    Thread.sleep(10000) //Time taken for path2 data to load
+
+    if(second_list==lis2.length){
+      print("Data loaded for PATH2")
+    }
+
+
+
 
   }
 
